98794753ebd1e9d8d2df4f5597cf8fa1100383e9
Cyrus <cyrus.kuznetsov@gmail.com>
2023-06-02 13:16:06 +0500

    build: react client setup

---
+++ client/vite.config.ts
@@ -0,0 +1,17 @@
+/* eslint-env node */
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    port: 5000,
+  },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
