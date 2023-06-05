9ca6c559dad401ee862fa134cbff8a627fc208b8
Cyrus <cyrus.kuznetsov@gmail.com>
2023-06-05 21:41:26 +0500

    fix: sanitize input

--- server/src/messages/dto/listen-as.dto.ts
+++ server/src/messages/dto/listen-as.dto.ts
@@ -1,7 +1,10 @@
 import { IsNotEmpty, IsString } from 'class-validator';
+import { sanitize } from 'isomorphic-dompurify';
+import { Transform } from 'class-transformer';
 
 export class ListenAsDto {
   @IsNotEmpty()
   @IsString()
+  @Transform(({ value }) => sanitize(value))
   name: string;
 }
--- server/src/messages/dto/send-message.dto.ts
+++ server/src/messages/dto/send-message.dto.ts
@@ -1,17 +1,23 @@
 import { IsNotEmpty, IsString } from 'class-validator';
+import { sanitize } from 'isomorphic-dompurify';
+import { Transform } from 'class-transformer';
 
 export class SendMessageDto {
   @IsNotEmpty()
   @IsString()
+  @Transform(({ value }) => sanitize(value))
   from: string;
 
   @IsNotEmpty()
   @IsString()
+  @Transform(({ value }) => sanitize(value))
   to: string;
 
   @IsString()
+  @Transform(({ value }) => sanitize(value))
   content: string;
 
   @IsString()
+  @Transform(({ value }) => sanitize(value))
   title: string;
 }
